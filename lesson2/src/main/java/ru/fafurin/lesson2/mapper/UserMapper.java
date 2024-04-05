2545cb3ddc605bdc430fbe2220c7384be0dd40a0
Fafurin <ffafurin@mail.ru>
2024-04-05 22:04:10 +0300

    The homework for the 2 lesson is completed

---
+++ lesson2/src/main/java/ru/fafurin/lesson2/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package ru.fafurin.lesson2.mapper;
+
+import org.springframework.jdbc.core.RowMapper;
+import ru.fafurin.lesson2.model.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper implements RowMapper<User> {
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        User user = new User();
+        user.setId(rs.getInt("id"));
+        user.setFirstName(rs.getString("firstName"));
+        user.setLastName(rs.getString("lastName"));
+        return user;
+    }
+}
