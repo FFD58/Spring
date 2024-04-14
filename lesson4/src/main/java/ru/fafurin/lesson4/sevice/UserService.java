ff40db90c31045edb79b21ad4640be1953fb77cd
Fafurin <ffafurin@mail.ru>
2024-04-14 13:35:45 +0300

    The homework for the 4 lesson is completed

---
+++ lesson4/src/main/java/ru/fafurin/lesson4/sevice/UserService.java
@@ -0,0 +1,36 @@
+package ru.fafurin.lesson4.sevice;
+
+import org.springframework.stereotype.Service;
+import ru.fafurin.lesson4.model.User;
+import ru.fafurin.lesson4.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(User user) {
+        return userRepository.update(user);
+    }
+
+    public void deleteById(Integer id) {
+        userRepository.deleteById(id);
+    }
+}
